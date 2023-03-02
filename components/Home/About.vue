9ec32cfbce62c6b2182ec212b15da9d0115091ae
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-03-02 16:32:57 +0100

    made changes

--- components/Home/About.vue
+++ components/Home/About.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="
-  bg-[url('https://images.pexels.com/photos/7293100/pexels-photo-7293100.jpeg?auto=compress&cs=tinysrgb&w=2160&h=1980&dpr=1')]
+  bg-[url('https://images.unsplash.com/photo-1549122648-a34a95c85f1d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2160&q=80')]
+  lg:flex flex-row-reverse justify-between items-center
   w-full h-fit bg-no-repeat text-white px-4 sm:px-8 md:px-12 lg:px-28 py-24">
-    <img class="" src="https://res.cloudinary.com/dsgvwxygr/image/upload/v1677768039/uacef/photo_2023-03-02_15-39-25_mtsmgl.jpg" alt="">
-    <h2 class="my-6 text-4xl font-semibold">Welcome to UACEF</h2>
-    <p class="my-6 text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum reiciendis assumenda odit repellat pariatur, quis sapiente explicabo, tempora reprehenderit libero commodi adipisci quaerat odio velit.</p>
-    <div class="block">
-      <NuxtLink class="block w-fit bg-purple-900 rounded-2xl px-8 py-4">Read More</NuxtLink>
+    <img class="lg:w-1/2" src="https://res.cloudinary.com/dsgvwxygr/image/upload/v1677768039/uacef/photo_2023-03-02_15-39-25_mtsmgl.jpg" alt="">
+    <div class="lg:w-1/2 lg:mx-8">
+      <h2 class="my-6 text-4xl md:text-5xl font-semibold">Welcome to UACEF</h2>
+      <p class="my-6 text-sm md:text-base">The United Akokwa Christian Elites Forum formerly Akokwa Christian Elites Forum was formed on 20th April 2000 as a mustard seed planted that has grown over the years to be a standing Iroko tree for the gospel of the kingdom of God in our land, state and nation at large. The group was formed out of a serious burden in the hearst of elders of the Christian body in our land. They noticed that christianity in our land and generation is filled with mediocrity with little or no exposures bringing limitations to the great commission as was mandated by our Lord and saviour Jesus Christ.</p>
+      <div class="block">
+        <NuxtLink class="block w-fit bg-purple-900 rounded-2xl px-8 py-4">Read More</NuxtLink>
+      </div>
     </div>
   </div>
 </template>
