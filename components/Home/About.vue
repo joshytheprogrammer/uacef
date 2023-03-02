5ce7e81a374d56aa4cab2adabeed0a413aaf01d6
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-03-02 16:38:31 +0100

    made changes

--- components/Home/About.vue
+++ components/Home/About.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="
-  bg-[url('https://images.unsplash.com/photo-1549122648-a34a95c85f1d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2160&q=80')]
+  bg-[url('https://images.unsplash.com/photo-1500206329404-5057e0aefa48?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2160&q=80')]
   lg:flex flex-row-reverse justify-between items-center
-  w-full h-fit bg-no-repeat text-white px-4 sm:px-8 md:px-12 lg:px-28 py-24">
+  w-full h-fit bg-no-repeat lg:bg-fixed text-white px-4 sm:px-8 md:px-12 lg:px-28 py-24">
     <img class="lg:w-1/2" src="https://res.cloudinary.com/dsgvwxygr/image/upload/v1677768039/uacef/photo_2023-03-02_15-39-25_mtsmgl.jpg" alt="">
     <div class="lg:w-1/2 lg:mx-8">
       <h2 class="my-6 text-4xl md:text-5xl font-semibold">Welcome to UACEF</h2>
