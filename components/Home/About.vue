9a449093fb6e6fb9df7d7305634fd18482da8699
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-03-02 16:07:06 +0100

    made changes

--- components/Home/About.vue
+++ components/Home/About.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="
-  bg-[url('https://images.pexels.com/photos/384553/pexels-photo-384553.jpeg?auto=compress&cs=tinysrgb&w=2160&h=750&dpr=1')]
-  w-full bg-no-repeat text-white px-4 sm:px-8 md:px-12 lg:px-28 py-24">
-    <img class="hover:scale transition-all" src="https://res.cloudinary.com/dsgvwxygr/image/upload/v1677768039/uacef/photo_2023-03-02_15-39-25_mtsmgl.jpg" alt="">
-    <h1></h1>
-    <p></p>
-    <button class="block"></button>
+  bg-[url('https://images.pexels.com/photos/7293100/pexels-photo-7293100.jpeg?auto=compress&cs=tinysrgb&w=2160&h=1980&dpr=1')]
+  w-full h-fit bg-no-repeat text-white px-4 sm:px-8 md:px-12 lg:px-28 py-24">
+    <img class="" src="https://res.cloudinary.com/dsgvwxygr/image/upload/v1677768039/uacef/photo_2023-03-02_15-39-25_mtsmgl.jpg" alt="">
+    <h2 class="my-6 text-4xl font-semibold">Welcome to UACEF</h2>
+    <p class="my-6 text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum reiciendis assumenda odit repellat pariatur, quis sapiente explicabo, tempora reprehenderit libero commodi adipisci quaerat odio velit.</p>
+    <div class="block">
+      <NuxtLink class="block w-fit bg-purple-900 rounded-2xl px-8 py-4">Read More</NuxtLink>
+    </div>
   </div>
 </template>
 
