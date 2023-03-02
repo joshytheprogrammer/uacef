e4e705b0594e8559cf258b7b9552a2dd18dd822e
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-03-02 14:41:01 +0100

    made changes

--- components/Home/Services.vue
+++ components/Home/Services.vue
@@ -11,15 +11,15 @@
       <figure class="mb-6">
         <Icon name="pixelarticons:teach" size="4.5em" />
       </figure>
-      <h3 class="text-2xl font-semibold">Teach the truth</h3>
-      <p class="mt-6 text-sm leading-7">After we have fed the body, we move on to more pressing matters - feeding the spirit. Teaching the gospel to unbelievers is one of our founding principles. </p>
+      <h3 class="text-2xl font-semibold">Spread the truth</h3>
+      <p class="mt-6 text-sm leading-7">After feeding the body, we move on to more pressing matters - feeding the spirit. Sharing the gospel to unbelievers is one of our founding principles. </p>
     </div>
     <div class="w-full sm:w-fit md:w-[49%] lg:w-[32.7%] rounded-2xl py-8 px-8 mb-4 flex flex-col items-center bg-violet-300 hover:bg-violet-900 text-black hover:text-white">
       <figure class="mb-6">
         <Icon name="carbon:friendship" size="4.5em" />
       </figure>
       <h3 class="text-2xl font-semibold">Fellowship together</h3>
-      <p class="mt-6 text-sm leading-7">After we have invited you into</p>
+      <p class="mt-6 text-sm leading-7">We have programmes of great magnitude hosting seasoned ministers of the gospel from all Christian divides. Several thousands are won to the kingdom yearly.</p>
     </div>
     
   </div>
