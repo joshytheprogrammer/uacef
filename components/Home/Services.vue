812a8dee014374665ff9c52f0bc07b798102497d
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-03-02 12:33:39 +0100

    made changes

--- components/Home/Services.vue
+++ components/Home/Services.vue
@@ -1,12 +1,27 @@
 <template>
-  <div class="flex justify-between text-center text-white px-8 md:px-12 lg:px-28 py-24">
-    <div class="w-full sm:w-fit md:w-1/2 lg:w-1/3 rounded-2xl py-8 px-8 flex flex-col items-center bg-gradient-to-r from-violet-900 to-purple-500">
-      <span>
-
-      </span>
-      <h3 class="text-xl font-semibold">Help the Needy</h3>
-      <p></p>
+  <div class="flex justify-between flex-wrap text-center text-white px-8 md:px-12 lg:px-28 py-24">
+    <div class="w-full sm:w-fit md:w-[49%] lg:w-[32.7%] rounded-2xl  py-8 px-8 mb-4 flex flex-col items-center bg-gradient-to-r from-violet-900 to-purple-500">
+      <figure class="mb-6">
+        <Icon name="mdi:handshake-outline" size="4.5em" />
+      </figure>
+      <h3 class="text-2xl font-semibold">Help the Needy</h3>
+      <p class="mt-6 text-sm leading-7">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet veritatis et explicabo ipsum impedit esse earum dolore.</p>
     </div>
+    <div class="w-full sm:w-fit md:w-[49%] lg:w-[32.7%] rounded-2xl py-8 px-8 mb-4 flex flex-col items-center bg-gradient-to-r from-violet-900 to-purple-500">
+      <figure class="mb-6">
+        <Icon name="mdi:handshake-outline" size="4.5em" />
+      </figure>
+      <h3 class="text-2xl font-semibold">Help the Needy</h3>
+      <p class="mt-6 text-sm leading-7">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet veritatis et explicabo ipsum impedit esse earum dolore.</p>
+    </div>
+    <div class="w-full sm:w-fit md:w-[49%] lg:w-[32.7%] rounded-2xl py-8 px-8 mb-4 flex flex-col items-center bg-gradient-to-r from-violet-900 to-purple-500">
+      <figure class="mb-6">
+        <Icon name="mdi:handshake-outline" size="4.5em" />
+      </figure>
+      <h3 class="text-2xl font-semibold">Help the Needy</h3>
+      <p class="mt-6 text-sm leading-7">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet veritatis et explicabo ipsum impedit esse earum dolore.</p>
+    </div>
+    
   </div>
 </template>
 
