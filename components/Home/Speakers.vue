261261fb1be93398aa3e4d6d629131ec4b29621e
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-03-03 12:20:32 +0100

    made changes

--- components/Home/Speakers.vue
+++ components/Home/Speakers.vue
@@ -33,11 +33,3 @@
     </div>
   </div>
 </template>
-
-<script setup>
-
-</script>
-
-<style lang="scss" scoped>
-
-</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
   app: {
     pageTransition: { name: 'page', mode: 'out-in' }
   },
-  ssr: false,
   modules: [
     '@nuxtjs/tailwindcss',
     '@pinia/nuxt',
