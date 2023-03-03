555c1002c39ec14896e500431b93de08c205ea96
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-03-03 10:27:28 +0100

    made changes

--- components/Home/Speakers.vue
+++ components/Home/Speakers.vue
@@ -1,10 +1,18 @@
 <template>
   <div class="
   bg-[url('https://images.unsplash.com/photo-1677530248563-e6105354fafb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2160&q=80')]
-  lg:flex flex-row-reverse justify-between items-center
-  w-full h-fit bg-no-repeat lg:bg-fixed text-white px-4 sm:px-8 md:px-12 lg:px-28 py-24
+  md:flex flex-col justify-between items-left
+  w-full h-fit bg-no-repeat lg:bg-fixed text-black px-4 sm:px-8 md:px-12 lg:px-28 py-24
   ">
+    <h1 class="
+    before:content-[''] before:absolute before:w-24 before:h-1 before:rounded-sm before:bg-purple-900 before:bottom-0 before:left-0
+    text-4xl md:text-5xl pb-6 font-semibold relative
+    ">Our Causes</h1>
+    <div class="">
+      <div class="">
 
+      </div>
+    </div>
   </div>
 </template>
 
