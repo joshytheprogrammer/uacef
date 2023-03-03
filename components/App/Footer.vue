38a65d361109213c1391447de6d38af6281a7e69
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-03-03 14:18:19 +0100

    made changes

--- components/App/Footer.vue
+++ components/App/Footer.vue
@@ -1,6 +1,20 @@
 <template>
-  <div>
-
+  <div class="w-full bg-white py-8 px-4 sm:px-8 md:px-12 lg:px-28 lg:py-32  flex items-center justify-between">
+    <div class="">
+      <NuxtLink class="flex items-center pb-4" to="/#">
+        <img class="w-32 pr-4" src="~/assets/img/uacef-logo.png" alt="UACEF Logo">
+        <span class="text-5xl text-purple-900 font-bold">UACEF</span>
+      </NuxtLink>
+      <p class="py-4 leading-7 lg:pb-12 md:w-full">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia recusandae culpa unde minima inventore repellat exercitationem eos impedit omnis quibusdam voluptate cum ullam, cumque, libero a deserunt odio saepe, adipisci ut ipsam? Quas autem ipsam illo ratione. Facilis dignissimos hic labore, impedit ea similique, magni, saepe natus distinctio voluptatibus exercitationem at? Iste porro hic quos voluptatibus error delectus praesentium officia?
+      </p>
+      <div class="">
+        
+      </div>
+    </div>
+    <div class=""></div>
+    <div class=""></div>
+    <div class=""></div>
   </div>
 </template>
 
