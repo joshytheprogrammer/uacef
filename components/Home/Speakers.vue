72b515888c9fef6d004f8e361a0618d9146c36c7
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-03-03 10:54:47 +0100

    made changes

--- components/Home/Speakers.vue
+++ components/Home/Speakers.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="
-  bg-[url('https://images.unsplash.com/photo-1677530248563-e6105354fafb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2160&q=80')]
+  bg-slate-200
   md:flex flex-col justify-between items-left
   w-full h-fit bg-no-repeat lg:bg-fixed text-black px-4 sm:px-8 md:px-12 lg:px-28 py-24
   ">
@@ -9,21 +9,21 @@
     text-4xl md:text-5xl pb-6 font-semibold relative
     ">Our Causes</h1>
     <div class="w-full md:flex justify-between pt-24 ">
-      <div class="rounded-2xl w-full sm:w-fit md:w-[49%] lg:w-[32.7%]">
+      <div class="rounded-2xl w-full mb-4 sm:w-fit md:w-[49%] lg:w-[32.7%]">
         <img class="rounded-t-2xl " src="https://i0.wp.com/topcelebrities.com.ng/wp-content/uploads/2021/08/IMG-20210801-WA0055.jpg?resize=465%2C310&ssl=1" alt="Help widows image">
         <div class="bg-white p-6">
           <h3 class="text-xl font-semibold">Help widows </h3>
           <p class="text-slate-500 mt-6 text-sm leading-7">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam voluptate, quas dignissimos odit blanditiis fugiat beatae aspernatur animi?</p>
         </div>
       </div>
-      <div class="rounded-2xl w-full sm:w-fit md:w-[49%] lg:w-[32.7%]">
+      <div class="rounded-2xl w-full mb-6 sm:w-fit md:w-[49%] lg:w-[32.7%]">
         <img class="rounded-t-2xl " src="https://i0.wp.com/topcelebrities.com.ng/wp-content/uploads/2021/08/IMG-20210801-WA0055.jpg?resize=465%2C310&ssl=1" alt="Help widows image">
         <div class="bg-white p-6">
           <h3 class="text-xl font-semibold">Help widows </h3>
           <p class="text-slate-500 mt-6 text-sm leading-7">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam voluptate, quas dignissimos odit blanditiis fugiat beatae aspernatur animi?</p>
         </div>
       </div>
-      <div class="rounded-2xl w-full sm:w-fit md:w-[49%] lg:w-[32.7%]">
+      <div class="rounded-2xl w-full mb-6 sm:w-fit md:w-[49%] lg:w-[32.7%]">
         <img class="rounded-t-2xl " src="https://i0.wp.com/topcelebrities.com.ng/wp-content/uploads/2021/08/IMG-20210801-WA0055.jpg?resize=465%2C310&ssl=1" alt="Help widows image">
         <div class="bg-white p-6">
           <h3 class="text-xl font-semibold">Help widows </h3>
