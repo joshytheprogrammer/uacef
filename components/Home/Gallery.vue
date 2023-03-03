8146797eb81de09e07091e1c878864b36321da3b
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-03-03 12:56:28 +0100

    made changes

--- components/Home/Gallery.vue
+++ components/Home/Gallery.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="md:columns-2 lg:columns-3 gap-6 px-4 sm:px-8 md:px-12 lg:px-28 py-24 transition-all">
     <figure class="w-full p-1 shadow-md mr-6 mb-6 rounded-xl inline-block border-2 border-solid border-gray-400 hover:border-purple-900" v-for="item in images" :key="item.id">
-      <img class="grayscale hover:grayscale-0 rounded-xl " :src="item.link" :alt="item.alt">
+      <img class="rounded-xl " :src="item.link" :alt="item.alt">
     </figure>
   </div>
 </template>
