bda59f43adb752d6aa6890764a481dbc926a4d9d
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-03-03 12:00:42 +0100

    made changes

--- components/Home/Speakers.vue
+++ components/Home/Speakers.vue
@@ -8,23 +8,23 @@
     before:content-[''] before:absolute before:w-24 before:h-1 before:rounded-sm before:bg-purple-900 before:bottom-0 before:left-0
     text-4xl md:text-5xl pb-6 font-semibold relative
     ">Our Causes</h1>
-    <div class="w-full md:flex justify-between pt-16">
-      <div class="rounded-2xl w-full mb-4 sm:w-fit md:w-[49%] lg:w-[32.7%]">
-        <img class="rounded-t-2xl " src="https://i0.wp.com/topcelebrities.com.ng/wp-content/uploads/2021/08/IMG-20210801-WA0055.jpg?resize=465%2C310&ssl=1" alt="Help widows image">
+    <div class="w-full md:flex flex-wrap justify-between pt-16">
+      <div class="rounded-t-2xl w-full mb-4 sm:w-fit md:w-[49%] lg:w-[32.7%]">
+        <img class="w-full rounded-t-2xl object-fill" src="https://i0.wp.com/topcelebrities.com.ng/wp-content/uploads/2021/08/IMG-20210801-WA0055.jpg?resize=465%2C310&ssl=1" alt="Help widows image">
         <div class="bg-white p-6">
           <h3 class="text-xl font-semibold">Providing for Widows</h3>
           <p class="text-slate-500 mt-6 text-sm leading-7">“Religion that God our Father accepts as pure and faultless is this: to look after orphans and widows in their distress and to keep oneself from being polluted by the world.”</p>
         </div>
       </div>
       <div class="rounded-2xl w-full mb-6 sm:w-fit md:w-[49%] lg:w-[32.7%]">
-        <img class="rounded-t-2xl " src="https://i0.wp.com/topcelebrities.com.ng/wp-content/uploads/2021/08/IMG-20210801-WA0055.jpg?resize=465%2C310&ssl=1" alt="Help widows image">
+        <img class="w-full rounded-t-2xl object-fill" src="https://images.unsplash.com/photo-1625481710487-b93a4142d0a2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80" alt="Helping those in need image">
         <div class="bg-white p-6">
           <h3 class="text-xl font-semibold">Helping those in need</h3>
           <p class="text-slate-500 mt-6 text-sm leading-7">"For there will never cease to be poor in the land. Therefore I command you, 'You shall open wide your hand to your brother, to the needy and to the poor, in your land.'"</p>
         </div>
       </div>
       <div class="rounded-2xl w-full mb-6 sm:w-fit md:w-[49%] lg:w-[32.7%]">
-        <img class="rounded-t-2xl " src="https://i0.wp.com/topcelebrities.com.ng/wp-content/uploads/2021/08/IMG-20210801-WA0055.jpg?resize=465%2C310&ssl=1" alt="Help widows image">
+        <img class="w-full rounded-t-2xl object-fill" src="https://images.unsplash.com/photo-1529390079861-591de354faf5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="Education for all image">
         <div class="bg-white p-6">
           <h3 class="text-xl font-semibold">Education for all</h3>
           <p class="text-slate-500 mt-6 text-sm leading-7">"My people are destroyed for lack of knowledge: because thou hast rejected knowledge, I will also reject thee, that thou shalt be no priest to me..."</p>
