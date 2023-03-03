fa5803b6b705c0c4a90b8b1dc040016e17dbc32f
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-03-03 10:02:43 +0100

    made changes

--- components/Home/Speakers.vue
+++ components/Home/Speakers.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>
+  <div class="
+  bg-[url('')]
+  lg:flex flex-row-reverse justify-between items-center
+  w-full h-fit bg-no-repeat lg:bg-fixed text-white px-4 sm:px-8 md:px-12 lg:px-28 py-24
+  ">
 
   </div>
 </template>
