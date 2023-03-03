27e9c0f55fec7400eceab0862b3b79d1244ab622
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-03-03 12:11:06 +0100

    made changes

--- components/Home/Jumbotron.vue
+++ components/Home/Jumbotron.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="
-  text-white py-8 px-4 sm:px-8 md:px-12 lg:px-28 flex flex-col justify-center bg-no-repeat bg-fixed h-fit md:h-[520px]
+  text-white py-8 px-4 sm:px-8 md:px-12 lg:px-28 lg:py-32 flex flex-col justify-center bg-no-repeat bg-fixed h-fit md:h-[520px]
   bg-[url('https://images.unsplash.com/photo-1609267257624-12a07857c7fe?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=978&q=80')] 
   md:bg-[url('https://images.unsplash.com/photo-1609267257624-12a07857c7fe?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1080&q=80')] 
   lg:bg-[url('https://images.unsplash.com/photo-1494832944834-a08818c634b0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=scale&w=1920&q=80')]">
@@ -8,7 +8,7 @@
       <h1 class="text-5xl lg:text-7xl font-bold">Building</h1>
       <h4 class="text-xl lg:text-4xl font-bold pt-2">4 a better Nigeria</h4>
     </div>
-    <p class="py-4 leading-6 lg:pb-12 md:w-full lg:w-1/2">The United Akokwa Christian Elites Forum is a non denominational forum with a special assignment for our land and our generation - Helping the needy, blessing the poor and putting smiles on the faces of our countrymen. We have been working at it for over <span class="font-semibold">2 DECADES</span> and we won't stop anytime soon. </p>
+    <p class="py-4 leading-7 lg:pb-12 md:w-full lg:w-1/2">The United Akokwa Christian Elites Forum is a non denominational forum with a special assignment for our land and our generation - Helping the needy, blessing the poor and putting smiles on the faces of our countrymen. We have been working at it for over <span class="font-semibold">2 DECADES</span> and we won't stop anytime soon. </p>
     <div class="flex pt-4 sm:pt-0">
       <NuxtLink to="/contact" class="block bg-purple-900 rounded-2xl px-8 py-4 mr-4">Join us</NuxtLink>
       <NuxtLink to="/about" class="block hover:bg-purple-900 border border-purple-900 rounded-2xl px-8 py-4 mr-4">Read More</NuxtLink>
--- components/Home/Speakers.vue
+++ components/Home/Speakers.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="
-  bg-slate-200
+  bg-purple-200
   md:flex flex-col justify-between items-left
   w-full h-fit bg-no-repeat lg:bg-fixed text-black px-4 sm:px-8 md:px-12 lg:px-28 py-24
   ">
@@ -10,14 +10,14 @@
     ">Our Causes</h1>
     <div class="w-full md:flex flex-wrap justify-between pt-16">
       <div class="rounded-t-2xl w-full mb-4 sm:w-fit md:w-[49%] lg:w-[32.7%]">
-        <img class="w-full rounded-t-2xl object-fill" src="https://i0.wp.com/topcelebrities.com.ng/wp-content/uploads/2021/08/IMG-20210801-WA0055.jpg?resize=465%2C310&ssl=1" alt="Help widows image">
+        <img class="w-full rounded-t-2xl object-fill" src="https://images.unsplash.com/photo-1522543558187-768b6df7c25c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="Help widows image">
         <div class="bg-white p-6">
           <h3 class="text-xl font-semibold">Providing for Widows</h3>
           <p class="text-slate-500 mt-6 text-sm leading-7">“Religion that God our Father accepts as pure and faultless is this: to look after orphans and widows in their distress and to keep oneself from being polluted by the world.”</p>
         </div>
       </div>
       <div class="rounded-2xl w-full mb-6 sm:w-fit md:w-[49%] lg:w-[32.7%]">
-        <img class="w-full rounded-t-2xl object-fill" src="https://images.unsplash.com/photo-1625481710487-b93a4142d0a2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80" alt="Helping those in need image">
+        <img class="w-full rounded-t-2xl object-fill" src="https://images.unsplash.com/photo-1625481710487-b93a4142d0a2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="Helping those in need image">
         <div class="bg-white p-6">
           <h3 class="text-xl font-semibold">Helping those in need</h3>
           <p class="text-slate-500 mt-6 text-sm leading-7">"For there will never cease to be poor in the land. Therefore I command you, 'You shall open wide your hand to your brother, to the needy and to the poor, in your land.'"</p>
