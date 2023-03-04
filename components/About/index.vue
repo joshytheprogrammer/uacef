e1c706bb075a8222b07fbe761ff70abee8d7af3e
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-03-04 05:09:31 +0100

    made changes

--- components/About/index.vue
+++ components/About/index.vue
@@ -2,8 +2,8 @@
   <div class="lg:flex flex-row-reverse justify-between items-center w-full h-fit bg-no-repeat lg:bg-fixed text-neutral-800 px-4 sm:px-8 md:px-12 py-16 lg:px-28 lg:py-24">
     <img class="lg:w-1/2" src="https://res.cloudinary.com/dsgvwxygr/image/upload/v1677832406/uacef/IMG-20210801-WA0048_lf5ria.jpg" alt="About page thumbnail">
     <div class="lg:w-1/2 lg:mr-8">
-      <h2 class="my-6 text-4xl md:text-5xl font-semibold">Welcome to UACEF</h2>
-      <p class="my-6 text-sm md:text-base md:leading-7">The United Akokwa Christian Elites Forum formerly Akokwa Christian Elites Forum was formed on 20th April 2000 as a mustard seed planted that has grown over the years to be a standing Iroko tree for the gospel of the kingdom of God in our land, state and nation at large. The group was formed out of a serious burden in the hearst of elders of the Christian body in our land. They noticed that christianity in our land and generation is filled with mediocrity with little or no exposures bringing limitations to the great commission as was mandated by our Lord and saviour Jesus Christ.</p>
+      <h2 class="my-6 text-2xl md:text-3xl font-semibold">Our Origins</h2>
+      <p class="my-6 text-sm leading-6 md:text-base md:leading-7"></p>
       <div class="block">
         <NuxtLink to="/about" class="block w-fit text-white bg-purple-900 rounded-2xl px-8 py-4">Read More</NuxtLink>
       </div>
