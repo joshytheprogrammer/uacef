cdbd7e57c412bf7d2c317a3f1470304ed8fcd5b6
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-03-04 04:55:24 +0100

    made changes

--- components/About/Copy.vue
+++ components/About/Copy.vue
@@ -1,10 +1,12 @@
 <template>
-  <div class="w-full ">
-    <div class="w-48%">
-
+  <div class="w-full flex lg:flex-row flex-col px-4 sm:px-8 md:px-12 lg:px-28 pb-24 ">
+    <div class="">
+      <h2 class="text-xl font-semibold">Mission</h2>
+      <p class="text-slate-500 my-6 text-sm leading-7">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio doloremque odit beatae quaerat eligendi maiores quas, ullam consequatur est dolorem? Ea maiores aspernatur officia repellat magni doloremque tenetur nihil alias nemo id fugit vel ipsa consectetur amet, at optio sed harum veniam quidem. Quia, eius?</p>
     </div>
-    <div>
-       
+    <div class="">
+      <h2 class="text-xl font-semibold">Vision</h2>
+      <p class="text-slate-500 my-6 text-sm leading-7">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas nesciunt et quis velit itaque commodi praesentium exercitationem laudantium quam, possimus, magnam perferendis! Quas expedita asperiores sed ratione soluta beatae autem, laborum nesciunt deserunt, deleniti architecto voluptas nemo quasi ut dicta repellat quaerat molestias iusto dolore?</p>
     </div>
   </div>
 </template>
