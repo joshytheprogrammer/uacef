f6951d0d1cdda0c2bf6e6e7964b2c0af647cdd1b
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-03-04 04:02:32 +0100

    made changes

--- components/About/Hero.vue
+++ components/About/Hero.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="bg-[url('https://images.unsplash.com/flagged/photo-1553368181-248aa8fd2516?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1755&q=80')] text-white py-8 px-4 sm:px-8 md:px-12 lg:px-28 lg:py-32 h-fit md:h-[265px] ">
+  <div class="bg-[url('https://images.unsplash.com/flagged/photo-1553368181-248aa8fd2516?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1755&q=80')] text-white py-8 px-4 sm:px-8 md:px-12 lg:px-28 lg:py-24 h-fit md:h-[265px] ">
+    <h1 class="before:content-[''] before:absolute before:w-24 before:h-1 before:rounded-sm before:bg-purple-900 before:bottom-0 before:left-0 text-2xl md:text-4xl pb-6 font-semibold relative ">Our Causes</h1>
 
   </div>
 </template>
