91a1c0e98a0b47c784233c602cfd7b0511f634aa
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-03-04 03:23:55 +0100

    made changes

--- components/App/Footer.vue
+++ components/App/Footer.vue
@@ -2,8 +2,8 @@
   <div class="w-full bg-white py-8 px-4 sm:px-8 md:px-12 lg:px-28 lg:py-16 flex flex-col lg:flex-row items-start justify-between">
     <div class="max-w-md">
       <NuxtLink class="flex items-center pb-4" to="/#">
-        <img class="w-16 pr-4" src="~/assets/img/uacef-logo.png" alt="UACEF Logo">
-        <span class="text-5xl text-purple-900 font-bold">UACEF</span>
+        <img class="w-20 pr-4" src="~/assets/img/uacef-logo.png" alt="UACEF Logo">
+        <span class="text-3xl text-purple-900 font-bold">UACEF</span>
       </NuxtLink>
       <p class="py-8 text-sm leading-7 lg:pb-12 md:w-full">
         After initial changes of days, the forum finally settled for 26th - 28th December every year. Several seasoned ministers have mounted the podium to speak on our lives and the land which have given rise to thousands of souls been worn yearly into the kingdom. Speakers like Rev. Prof Most Madugba(International president, Ministers prayer network), Rev Dr. Charles Osueke(former GS Assemblies of God international), Bishop E.O.Ikeakor(Bishop Amichi Diocese Anglican communion), Witness Ken Paul Obieke, Rev Dr Emma Ifeadike etc
