c24dad94f533726a55608ff65b948ff2f115279a
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-03-04 04:12:50 +0100

    made changes

--- components/About/index.vue
+++ components/About/index.vue
@@ -1,5 +1,16 @@
 <template>
-  <div>
-
+  <div class="bg-[url('https://images.unsplash.com/photo-1500206329404-5057e0aefa48?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2160&q=80')] lg:flex flex-row-reverse justify-between items-center w-full h-fit bg-no-repeat lg:bg-fixed text-white px-4 sm:px-8 md:px-12 lg:px-28 py-24">
+    <img class="lg:w-1/2" src="https://res.cloudinary.com/dsgvwxygr/image/upload/v1677832407/uacef/IMG-20210801-WA0027-548x365_q7fprw.jpg" alt="About page thumbnail">
+    <div class="lg:w-1/2 lg:mr-8">
+      <h2 class="my-6 text-4xl md:text-5xl font-semibold">Welcome to UACEF</h2>
+      <p class="my-6 text-sm md:text-base md:leading-7">The United Akokwa Christian Elites Forum formerly Akokwa Christian Elites Forum was formed on 20th April 2000 as a mustard seed planted that has grown over the years to be a standing Iroko tree for the gospel of the kingdom of God in our land, state and nation at large. The group was formed out of a serious burden in the hearst of elders of the Christian body in our land. They noticed that christianity in our land and generation is filled with mediocrity with little or no exposures bringing limitations to the great commission as was mandated by our Lord and saviour Jesus Christ.</p>
+      <div class="block">
+        <NuxtLink to="/about" class="block w-fit bg-purple-900 rounded-2xl px-8 py-4">Read More</NuxtLink>
+      </div>
+    </div>
   </div>
 </template>
+
+<script setup>
+
+</script>
--- pages/about.vue
+++ pages/about.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
     <AboutHero />
+    <About />
   </div>
 </template>
