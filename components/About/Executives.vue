f34587e560c909b27a3e786e92232835f8067739
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-03-05 21:14:06 +0100

    made changes

--- components/About/Executives.vue
+++ components/About/Executives.vue
@@ -1,24 +1,39 @@
 <template>
-  <div id="executives" class="flex overflow-auto text-center text-white px-4 sm:px-8 md:px-12 lg:px-28 py-24">
-    <div class="w-full sm:w-fit md:w-[49%] lg:w-[32.7%] rounded-2xl py-8 px-8 mb-4 flex flex-col items-center bg-gradient-to-r from-purple-500 to-purple-900">
-      <h3 class="text-2xl font-semibold">Engr. Chidi Njaka</h3>
-      <p class="mt-6 text-sm leading-7">National President.</p>
+  <div id="executives" class="flex justify-between flex-wrap text-center text-white px-4 sm:px-8 md:px-12 lg:px-28 pb-24">
+    <div class="w-full sm:w-fit md:w-[49%] lg:w-[32.7%] rounded-2xl py-8 px-8 mb-4 flex flex-col items-center bg-purple-900">
+      <figure class="mb-6 w-24">
+        <img class="rounded-full" src="https://images.unsplash.com/photo-1597223557154-721c1cecc4b0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80" alt="">
+      </figure>
+      <h3 class="text-lg font-medium">Engr. Chidi Njaka</h3>
+      <p class="mt-2 text-sm leading-7">National President.</p>
     </div>
-    <div class="w-full sm:w-fit md:w-[49%] lg:w-[32.7%] rounded-2xl py-8 px-8 mb-4 ml-4 flex flex-col items-center bg-violet-300 hover:bg-purple-900 text-neutral-800 hover:text-white transition-all">
-      <h3 class="text-2xl font-semibold">Mr Clifford Onyeje</h3>
-      <p class="mt-6 text-sm leading-7">National Vice President</p>
+    <div class="w-full sm:w-fit md:w-[49%] lg:w-[32.7%] rounded-2xl py-8 px-8 mb-4 flex flex-col items-center bg-purple-900 text-white transition-all">
+      <figure class="mb-6 w-24">
+        <img class="rounded-full" src="https://images.unsplash.com/photo-1597223557154-721c1cecc4b0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80" alt="">
+      </figure>
+      <h3 class="text-lg font-medium">Mr Clifford Onyeje</h3>
+      <p class="mt-2 text-sm leading-7">National Vice President</p>
     </div>
-    <div class="w-full sm:w-fit md:w-[49%] lg:w-[32.7%] rounded-2xl py-8 px-8 mb-4 ml-4 flex flex-col items-center bg-violet-300 hover:bg-purple-900 text-neutral-800 hover:text-white transition-all">
-      <h3 class="text-2xl font-semibold">Surveyor Ejike Onwuchuluba</h3>
-      <p class="mt-6 text-sm leading-7">National Treasurer.</p>
+    <div class="w-full sm:w-fit md:w-[49%] lg:w-[32.7%] rounded-2xl py-8 px-8 mb-4 flex flex-col items-center bg-purple-900 text-white transition-all">
+      <figure class="mb-6 w-24">
+        <img class="rounded-full" src="https://images.unsplash.com/photo-1597223557154-721c1cecc4b0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80" alt="">
+      </figure>
+      <h3 class="text-lg font-medium">Surveyor Ejike Onwuchuluba</h3>
+      <p class="mt-2 text-sm leading-7">National Treasurer.</p>
     </div>
-    <div class="w-full sm:w-fit md:w-[49%] lg:w-[32.7%] rounded-2xl py-8 px-8 mb-4 ml-4 flex flex-col items-center bg-violet-300 hover:bg-purple-900 text-neutral-800 hover:text-white transition-all">
-      <h3 class="text-2xl font-semibold">Elder John Onyegiri</h3>
-      <p class="mt-6 text-sm leading-7">Chairman board of trustees.</p>
+    <div class="w-full sm:w-fit md:w-[49%] lg:w-[32.7%] rounded-2xl py-8 px-8 mb-4 flex flex-col items-center bg-purple-900 text-white transition-all">
+      <figure class="mb-6 w-24">
+        <img class="rounded-full" src="https://images.unsplash.com/photo-1597223557154-721c1cecc4b0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80" alt="">
+      </figure>
+      <h3 class="text-lg font-medium">Elder John Onyegiri</h3>
+      <p class="mt-2 text-sm leading-7">Chairman board of trustees.</p>
     </div>
-    <div class="w-full sm:w-fit md:w-[49%] lg:w-[32.7%] rounded-2xl py-8 px-8 mb-4 ml-4 flex flex-col items-center bg-violet-300 hover:bg-purple-900 text-neutral-800 hover:text-white transition-all">
-      <h3 class="text-2xl font-semibold">Mr.Kingsley Agbadike </h3>
-      <p class="mt-6 text-sm leading-7">National Secretary.</p>
+    <div class="w-full sm:w-fit md:w-[49%] lg:w-[32.7%] rounded-2xl py-8 px-8 mb-4 flex flex-col items-center bg-purple-900 text-white transition-all">
+      <figure class="mb-6 w-24">
+        <img class="rounded-full" src="https://images.unsplash.com/photo-1597223557154-721c1cecc4b0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80" alt="">
+      </figure>
+      <h3 class="text-lg font-medium">Mr Kingsley Agbadike </h3>
+      <p class="mt-2 text-sm leading-7">National Secretary.</p>
     </div>
   </div>
 </template>
