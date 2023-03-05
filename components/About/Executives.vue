37e7860cf80edaa82729e2c54ac012f7f020f3fc
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-03-05 21:22:47 +0100

    made changes

--- components/About/Executives.vue
+++ components/About/Executives.vue
@@ -1,35 +1,35 @@
 <template>
   <div id="executives" class="flex justify-start gap-2 flex-wrap text-center text-white px-4 sm:px-8 md:px-12 lg:px-28 pb-24">
     <div class="w-full sm:w-fit md:w-[49%] lg:w-[32.7%] rounded-2xl py-8 px-8 mb-4 flex flex-col items-center bg-purple-900">
-      <figure class="mb-6 w-24">
+      <figure class="mb-6 w-24 border-2 rounded-full p-1">
         <img class="rounded-full" src="https://images.unsplash.com/photo-1597223557154-721c1cecc4b0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80" alt="">
       </figure>
       <h3 class="text-lg font-medium">Engr. Chidi Njaka</h3>
       <p class="mt-2 text-sm leading-7">National President.</p>
     </div>
     <div class="w-full sm:w-fit md:w-[49%] lg:w-[32.7%] rounded-2xl py-8 px-8 mb-4 flex flex-col items-center bg-purple-900 text-white transition-all">
-      <figure class="mb-6 w-24">
+      <figure class="mb-6 w-24 border-2 rounded-full p-1">
         <img class="rounded-full" src="https://images.unsplash.com/photo-1597223557154-721c1cecc4b0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80" alt="">
       </figure>
       <h3 class="text-lg font-medium">Mr Clifford Onyeje</h3>
       <p class="mt-2 text-sm leading-7">National Vice President</p>
     </div>
     <div class="w-full sm:w-fit md:w-[49%] lg:w-[32.7%] rounded-2xl py-8 px-8 mb-4 flex flex-col items-center bg-purple-900 text-white transition-all">
-      <figure class="mb-6 w-24">
+      <figure class="mb-6 w-24 border-2 rounded-full p-1">
         <img class="rounded-full" src="https://images.unsplash.com/photo-1597223557154-721c1cecc4b0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80" alt="">
       </figure>
       <h3 class="text-lg font-medium">Surveyor Ejike Onwuchuluba</h3>
       <p class="mt-2 text-sm leading-7">National Treasurer.</p>
     </div>
     <div class="w-full sm:w-fit md:w-[49%] lg:w-[32.7%] rounded-2xl py-8 px-8 mb-4 flex flex-col items-center bg-purple-900 text-white transition-all">
-      <figure class="mb-6 w-24">
+      <figure class="mb-6 w-24 border-2 rounded-full p-1">
         <img class="rounded-full" src="https://images.unsplash.com/photo-1597223557154-721c1cecc4b0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80" alt="">
       </figure>
       <h3 class="text-lg font-medium">Elder John Onyegiri</h3>
       <p class="mt-2 text-sm leading-7">Chairman board of trustees.</p>
     </div>
     <div class="w-full sm:w-fit md:w-[49%] lg:w-[32.7%] rounded-2xl py-8 px-8 mb-4 flex flex-col items-center bg-purple-900 text-white transition-all">
-      <figure class="mb-6 w-24">
+      <figure class="mb-6 w-24 border-2 rounded-full p-1">
         <img class="rounded-full" src="https://images.unsplash.com/photo-1597223557154-721c1cecc4b0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80" alt="">
       </figure>
       <h3 class="text-lg font-medium">Mr Kingsley Agbadike </h3>
