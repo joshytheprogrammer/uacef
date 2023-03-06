fb4e86d836a6558060a9b9efe9d0cee82c6c1fd1
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-03-06 15:59:30 +0100

    made changes

--- components/Causes/Copy.vue
+++ components/Causes/Copy.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="w-full lg:flex lg:text-center justify-center px-4 sm:px-8 md:px-12 lg:px-28 pt-16">
-    <p class="text-black my-6 text-lg leading-7 lg:w-1/2 md:text-2xl md:leading-9">
+    <p class="text-black text-lg leading-7 lg:w-1/2 md:text-2xl md:leading-9">
       "<b>UACEF</b> is dedicated to causes that liberate Nigerians from the shackles of poverty and directs men to the light of the gospel."
     </p>
   </div>
