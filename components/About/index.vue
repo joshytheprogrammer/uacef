95650ff8bae751f9dbc6971e6a797f62080f95d2
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-03-06 14:12:46 +0100

    made changes

--- components/About/index.vue
+++ components/About/index.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="lg:flex flex-row-reverse justify-between items-center w-full h-fit bg-no-repeat lg:bg-fixed text-neutral-800 px-4 sm:px-8 md:px-12 py-16 lg:px-28 lg:py-24">
-    <img class="lg:w-1/2" src="https://res.cloudinary.com/dsgvwxygr/image/upload/v1677832406/uacef/IMG-20210801-WA0048_lf5ria.jpg" alt="About page thumbnail">
+    <img class="lg:w-1/2" src="https://res.cloudinary.com/dsgvwxygr/image/upload/v1677832406/uacef/IMG-20210801-WA0048_lf5ria.jpg" alt="UACEF about page thumbnail - Great men gathered">
     <div class="lg:w-1/2 lg:mr-8">
       <h2 class="my-6 text-2xl md:text-3xl font-semibold">Our Origins</h2>
-      <p class="text-slate-500 my-6 text-sm leading-6 md:text-base md:leading-7">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed laborum ipsa, impedit officia doloribus quos molestiae optio! Repellendus, vero esse dolorem natus cum sint ex nesciunt minus itaque atque quis!</p>
+      <p class="text-slate-500 my-6 text-sm leading-6 md:text-base md:leading-7">The <b>United Akokwa Christian Elites Forum</b> formerly known as Akokwa Christian Elites Forum was formed on <b>20th April, 2000</b> as a mustard seed planted that has grown over the years to be a standing Iroko tree for the gospel of the kingdom of God in our land, state and Nation at large.</p>
       <div class="block">
         <NuxtLink to="/about#history" class="block w-fit text-white bg-purple-900 rounded-2xl px-8 py-4">Read More</NuxtLink>
       </div>
