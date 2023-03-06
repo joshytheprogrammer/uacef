c0f0c0e2f890b3c6856da3a30a18b3e8de6e0d74
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-03-06 17:14:54 +0100

    made changes

--- components/Causes/Copy.vue
+++ components/Causes/Copy.vue
@@ -1,7 +1,30 @@
 <template>
-  <div class="w-full lg:flex lg:text-center justify-center px-4 sm:px-8 md:px-12 lg:px-28 pt-16">
-    <p class="text-black text-lg leading-7 lg:w-1/2 md:text-2xl md:leading-9">
+  <div class="w-full px-4 sm:px-8 md:px-12 lg:px-28 pt-16">
+    <p class="text-center lg:py-12 text-black text-lg leading-7 md:text-2xl md:leading-9">
       "<b>UACEF</b> is dedicated to causes that liberate Nigerians from the shackles of poverty and directs men to the light of the gospel."
     </p>
+    <div class="w-full md:flex flex-wrap justify-between pt-16">
+      <div class="rounded-t-2xl w-full mb-4 sm:w-fit md:w-[49%] lg:w-[32.7%]">
+        <img class="w-full rounded-t-2xl object-fill" src="https://images.unsplash.com/photo-1522543558187-768b6df7c25c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="Help widows image">
+        <div class="bg-white p-6">
+          <h3 class="text-xl font-semibold">Providing for Widows</h3>
+          <p class="text-slate-500 mt-6 text-sm leading-7">“Religion that God our Father accepts as pure and faultless is this: to look after orphans and widows in their distress and to keep oneself from being polluted by the world.”</p>
+        </div>
+      </div>
+      <div class="rounded-t-2xl w-full mb-6 sm:w-fit md:w-[49%] lg:w-[32.7%]">
+        <img class="w-full rounded-t-2xl object-fill" src="https://res.cloudinary.com/dsgvwxygr/image/upload/q_100/v1677861520/uacef/bg-img-1_a9pp2c.jpg" alt="Helping those in need image">
+        <div class="bg-white p-6">
+          <h3 class="text-xl font-semibold">Helping those in need</h3>
+          <p class="text-slate-500 mt-6 text-sm leading-7">"For there will never cease to be poor in the land. Therefore I command you, 'You shall open wide your hand to your brother, to the needy and to the poor, in your land.'"</p>
+        </div>
+      </div>
+      <div class="rounded-t-2xl w-full mb-6 sm:w-fit md:w-[49%] lg:w-[32.7%]">
+        <img class="w-full rounded-t-2xl object-fill" src="https://images.unsplash.com/photo-1529390079861-591de354faf5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="Education for all image">
+        <div class="bg-white p-6">
+          <h3 class="text-xl font-semibold">Education for all</h3>
+          <p class="text-slate-500 mt-6 text-sm leading-7">"My people are destroyed for lack of knowledge: because thou hast rejected knowledge, I will also reject thee, that thou shalt be no priest to me..."</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
