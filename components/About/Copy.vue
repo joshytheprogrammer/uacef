72a9ba5e6e6faa20a6ef79109af9f61d76040ff5
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-03-06 14:31:00 +0100

    made changes

--- components/About/Copy.vue
+++ components/About/Copy.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="w-full flex lg:flex-row flex-col px-4 sm:px-8 md:px-12 lg:px-28 pt-24">
-    <div class="lg:pr-4">
+    <div class="lg:w-1/2">
       <h2 class="text-2xl font-semibold">Our Mission</h2>
-      <p class="text-slate-500 my-6 text-sm leading-7 md:text-base md:leading-7">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio doloremque odit beatae quaerat eligendi maiores quas, ullam consequatur est dolorem? Ea maiores aspernatur officia repellat magni doloremque tenetur nihil alias nemo id fugit vel ipsa consectetur amet, at optio sed harum veniam quidem. Quia, eius?</p>
+      <p class="text-slate-500 my-6 text-sm leading-7 md:text-base md:leading-7">UACEF was a unique assignment and a herculean task for the few brethren who caught the vision considering our people were already awashed with mediocrity. But the team did what can be considered to be spiritual expedient by putting down the high points in writing according to Habakkuk 2:2 so as to run with it, those who find it may read and understand it better.</p>
     </div>
-    <div class="lg:pl-4">
+    <div class="lg:w-1/2 lg:pl-4">
       <h2 class="text-2xl font-semibold">Our Vision</h2>
-      <p class="text-slate-500 my-6 text-sm leading-7 md:text-base md:leading-7">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas nesciunt et quis velit itaque commodi praesentium exercitationem laudantium quam, possimus, magnam perferendis! Quas expedita asperiores sed ratione soluta beatae autem, laborum nesciunt deserunt, deleniti architecto voluptas nemo quasi ut dicta repellat quaerat molestias iusto dolore?</p>
+      <p class="text-slate-500 my-6 text-sm leading-7 md:text-base md:leading-7">Late Mr O.C. Emenike and a few of his executives were mandated to format a credible and unique forum that will bring together the elitist christians in our land. They would help to spread the gospel of the kingdom mostly to the upper and lower echelon of the society among the political leadership, opinion moulders, the traditional leadership, the business men's and women in our society, the indigent, the brokenhearted, the widows and the helpless among us.</p>
     </div>
   </div>
 </template>
--- components/About/index.vue
+++ components/About/index.vue
@@ -3,7 +3,7 @@
     <img class="lg:w-1/2" src="https://res.cloudinary.com/dsgvwxygr/image/upload/v1677832406/uacef/IMG-20210801-WA0048_lf5ria.jpg" alt="UACEF about page thumbnail - Great men gathered">
     <div class="lg:w-1/2 lg:mr-8">
       <h2 class="my-6 text-2xl md:text-3xl font-semibold">Our Origins</h2>
-      <p class="text-slate-500 my-6 text-sm leading-6 md:text-base md:leading-7">The <b>United Akokwa Christian Elites Forum</b> formerly known as Akokwa Christian Elites Forum was formed on <b>20th April, 2000</b> as a mustard seed planted that has grown over the years to be a standing Iroko tree for the gospel of the kingdom of God in our land, state and Nation at large.</p>
+      <p class="text-slate-500 my-6 text-sm leading-6 md:text-base md:leading-7">The <b>United Akokwa Christian Elites Forum</b> formerly known as Akokwa Christian Elites Forum was formed on <b>20th April, 2000</b> as a mustard seed planted that has grown over the years to be a standing Iroko tree for the gospel of the kingdom of God in our land, state and Nation at large. Hence the Elders of our village prayerfully formatted this group with Late Mr O.C. Emenike, an astute banker and stock broker as the pioneer president. </p>
       <div class="block">
         <NuxtLink to="/about#history" class="block w-fit text-white bg-purple-900 rounded-2xl px-8 py-4">Read More</NuxtLink>
       </div>
