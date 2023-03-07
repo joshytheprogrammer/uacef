302e2fe6bb70de1236c1df819ef425522e4df3f8
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-03-07 18:11:06 +0100

    made changes

--- components/Gallery/index.vue
+++ components/Gallery/index.vue
@@ -16,41 +16,6 @@
             alt: 'img-1',
             link: 'https://res.cloudinary.com/dsgvwxygr/image/upload/v1677832846/uacef/photo_2023-03-03_09-37-15_pxcryi.jpg'
           },
-          {
-            id: 1,
-            alt: 'img-2',
-            link: 'https://res.cloudinary.com/dsgvwxygr/image/upload/v1677832846/uacef/photo_2023-03-03_09-37-09_r4qh5c.jpg'
-          },
-          {
-            id: 3,
-            alt: 'img-4',
-            link: 'https://res.cloudinary.com/dsgvwxygr/image/upload/v1677832846/uacef/photo_2023-03-03_09-36-58_hhp75h.jpg'
-          },
-          {
-            id: 4,
-            alt: 'img-5',
-            link: 'https://res.cloudinary.com/dsgvwxygr/image/upload/v1677832846/uacef/photo_2023-03-03_09-36-46_dyaens.jpg'
-          },
-          {
-            id: 5,
-            alt: 'img-6',
-            link: 'https://res.cloudinary.com/dsgvwxygr/image/upload/v1677832846/uacef/photo_2023-03-03_09-36-53_gwik4r.jpg'
-          },
-          {
-            id: 6,
-            alt: 'img-7',
-            link: 'https://res.cloudinary.com/dsgvwxygr/image/upload/v1677832846/uacef/photo_2023-03-03_09-36-43_gpwjcm.jpg'
-          },
-          {
-            id: 7,
-            alt: 'img-8',
-            link: 'https://res.cloudinary.com/dsgvwxygr/image/upload/v1677832845/uacef/photo_2023-03-03_09-36-39_mvp7xl.jpg'
-          },
-          {
-            id: 8,
-            alt: 'img-9',
-            link: 'https://res.cloudinary.com/dsgvwxygr/image/upload/v1677832845/uacef/photo_2023-03-03_09-36-27_yf1dph.jpg'
-          },
         ]
       }
     }
