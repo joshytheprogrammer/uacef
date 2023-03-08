6f84b170ca091e267ac1539bece4531956d318ad
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-03-09 00:47:29 +0100

    fixed ogp socials issue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,13 @@ export default defineNuxtConfig({
       viewport: 'width=500, initial-scale=1',
       title: 'UACEF - The United Akokwa Christian Elites Forum.',
       meta: [
-        { name: 'description', content: 'The United Akokwa Christian Elites Forum formerly Akokwa Christian Elites Forum was formed on 20th April 2000 as a mustard seed planted that has grown over the years to be a standing Iroko tree for the gospel of the kingdom of God in our land, state and nation at large.' }
+        { name: 'description', content: 'The United Akokwa Christian Elites Forum is a non denominational forum with a special assignment for our land and our generation - Helping the needy, blessing the poor and putting smiles on the faces of our countrymen. We have been working at it for over 2 DECADES and we won\'t stop anytime soon.' },
+        { name: 'format-detection', content: 'telephone=no' },
+        { property: 'og:site_name', content: "UACEF"},
+        { hid: 'og:title', property: 'og:title', content: 'UACEF - The United Akokwa Christian Elites Forum.'},
+        { hid: 'og:description', property: 'og:description', content: 'The United Akokwa Christian Elites Forum is a non denominational forum with a special assignment for our land and our generation - Helping the needy, blessing the poor and putting smiles on the faces of our countrymen. We have been working at it for over 2 DECADES and we won\'t stop anytime soon.'},
+        { hid: 'og:image', property: 'og:image', content: 'https://res.cloudinary.com/dsgvwxygr/image/upload/v1678319143/uacef/uacef-logo_v9ocs6.png'},
+        { hid: 'og:url', property: 'og:url', content: 'https://uacef.demo.joshytheprogrammer.com'},
       ],
     }
   },
