e29f7c8ddb5b6e6aaf640a71df4f5abce496bd10
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-03-08 09:00:28 +0100

    made changes

--- components/Contact/Hero.vue
+++ components/Contact/Hero.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
-
+  <div class="bg-[url('https://res.cloudinary.com/dsgvwxygr/image/upload/v1678261530/uacef/contact_bg_szdqbw.jpg')] lg:bg-[url('https://res.cloudinary.com/dsgvwxygr/image/upload/c_scale,h_445,w_1880/v1678261530/uacef/contact_bg_szdqbw.jpg')] text-white flex items-center py-8 px-4 sm:px-8 md:px-12 lg:px-28 lg:py-24 h-fit md:h-[265px] ">
+    <h1 class="before:content-[''] before:absolute before:w-32 before:h-1 before:rounded-sm before:bg-purple-900 before:bottom-0 before:left-0 text-2xl md:text-4xl pb-6 font-semibold relative ">Contact us</h1>
   </div>
 </template>
