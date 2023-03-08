e78857bd77a1dde325fbec9d67a284537a72cecc
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-03-08 10:51:41 +0100

    made changes

--- components/Contact/Maps.vue
+++ components/Contact/Maps.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-
+    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d31713.19989732525!2d3.3642907999999996!3d6.502689449999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sng!4v1678266019267!5m2!1sen!2sng" width="100%" height="450" style="border:0;" allowfullscreen="false" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
   </div>
 </template>
