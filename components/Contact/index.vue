54fcbe7ab30323b9d2367326c8d092438359c071
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-03-08 09:08:27 +0100

    made changes

--- components/Contact/index.vue
+++ components/Contact/index.vue
@@ -1,5 +1,39 @@
 <template>
-  <div>
+  <div class="lg:flex justify-between items-center w-full h-fit bg-no-repeat lg:bg-fixed text-neutral-800 px-4 sm:px-8 md:px-12 py-16 lg:px-28 lg:py-24">
+    <div class="">
+      <h2>Get in touch with us</h2>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid dolore vitae, itaque voluptate blanditiis tempore quod laborum reiciendis molestiae quasi rerum maiores officiis incidunt sapiente error quisquam fuga enim quis.</p>
+      <ul class="flex w-full pb-8">
+        <li class="pr-4">
+          <Icon name="cib:facebook" size="1.25em" />
+        </li>
+        <li class="pr-4">
+          <Icon name="cib:instagram" size="1.25em" />
+        </li>
+        <li class="pr-4">
+          <Icon name="cib:twitter" size="1.25em" />
+        </li>
+        <li class="pr-4">
+          <Icon name="cib:pinterest" size="1.25em" />
+        </li>
+      </ul>
+      <ul class="py-8">
+        <li class="pb-8 ">
+          <Icon class="pr-1 text-purple-900" name="material-symbols:phone-in-talk" size="1.25em" />
+          <span class="pr-4">+234 345 678 901 </span>
+        </li>
+        <li class="pb-8">
+          <Icon class="pr-1 text-purple-900"  name="material-symbols:mail-rounded" size="1.25em" />
+          <span class="pr-4">contact@ourcharity.com</span>
+        </li>
+        <li class="pb-8">
+          <Icon class="pr-1 text-purple-900"  name="material-symbols:location-on" size="1.25em" />
+          <span class="pr-4">No 45-46, b3, 56832, Los Angeles, CA</span>
+        </li>
+      </ul>
+    </div>
+    <div class="bg-purple-300 ">
 
+    </div>
   </div>
 </template>
