78864b8885d62010ab652e16a5eaf255271f5832
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-03-09 22:34:36 +0100

    fixed issue with causes card

--- components/Causes/index.vue
+++ components/Causes/index.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="bg-purple-200 md:flex flex-col justify-between items-left w-full h-fit bg-no-repeat lg:bg-fixed text-neutral-800 px-4 sm:px-8 md:px-12 lg:px-28 py-24">
     <div class="w-full md:flex flex-wrap justify-between">
-      <div class="rounded-t-2xl transition-all hover:shadow-md w-full mb-6 sm:w-fit md:w-[49%] lg:w-[32.7%]" v-for="item in data" :key="item.id">
+      <div class="rounded-t-2xl bg-white transition-all hover:shadow-md w-full mb-6 sm:w-fit md:w-[49%] lg:w-[32.7%]" v-for="item in data" :key="item.id">
         <img class="w-full rounded-t-2xl object-fill" :src="item.imgLink" :alt="item.name">
-        <div class="bg-white p-6">
+        <div class="p-6">
           <h3 class="text-xl font-semibold"> {{ item.name }} </h3>
           <p class="text-slate-500 mt-6 text-sm leading-7"> {{ item.desc }} </p>
         </div>
