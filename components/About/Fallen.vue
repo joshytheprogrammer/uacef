589e1330b3e00d366d72f5fb5378fba2dbe7b871
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-03-13 16:07:11 +0100

    adding fallen heroes section

--- components/About/Fallen.vue
+++ components/About/Fallen.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="flex justify-start gap-2 px-4 sm:px-8 md:px-12 lg:px-28 pb-16 transition-all">
+  <div class="flex justify-start flex-wrap gap-2 px-4 sm:px-8 md:px-12 lg:px-28 pb-16 transition-all">
     <div class="rounded-t-2xl bg-white transition-all hover:shadow-md w-full mb-6 sm:w-fit ">
-      <img class="w-full rounded-t-2xl object-fill h-96" :src="'https://res.cloudinary.com/dsgvwxygr/image/upload/v1678715023/photo_2023-03-13_14-43-12_lpr53t.jpg'" :alt="'image - Late Engr Jojo Udensi'">
+      <img class="w-full rounded-t-2xl object-fill md:h-96" :src="'https://res.cloudinary.com/dsgvwxygr/image/upload/v1678715023/photo_2023-03-13_14-43-12_lpr53t.jpg'" :alt="'image - Late Engr Jojo Udensi'">
       <div class="p-6">
         <h3 class="text-xl font-semibold">Late Engr Jojo Udensi</h3>
         <p class="text-slate-500 mt-6 text-sm leading-7">Director of Evangelism Lagos Congress</p>
       </div>
     </div>
     <div class="rounded-t-2xl bg-white transition-all hover:shadow-md w-full mb-6 sm:w-fit ">
-      <img class="w-full rounded-t-2xl object-fill h-96" :src="'https://res.cloudinary.com/dsgvwxygr/image/upload/v1678715022/photo_2023-03-13_14-43-14_umy2c7.jpg'" :alt="'image - Late Okey Emenike'">
+      <img class="w-full rounded-t-2xl object-fill md:h-96" :src="'https://res.cloudinary.com/dsgvwxygr/image/upload/v1678715022/photo_2023-03-13_14-43-14_umy2c7.jpg'" :alt="'image - Late Okey Emenike'">
       <div class="p-6">
         <h3 class="text-xl font-semibold">Late Okey Emenike</h3>
         <p class="text-slate-500 mt-6 text-sm leading-7">First National President President</p>
