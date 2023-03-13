b19e4492451205e8b3e389c56b8670e401d8268b
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-03-13 15:04:54 +0100

    adding fallen heroes section

--- components/About/Fallen.vue
+++ components/About/Fallen.vue
@@ -1,21 +1,17 @@
 <template>
-  <div class="md:columns-2 lg:columns-3 gap-6 px-4 sm:px-8 md:px-12 lg:px-28 py-24 transition-all">
-    <div class="w-full md:flex flex-wrap justify-between">
-      <div class="rounded-t-2xl bg-white transition-all hover:shadow-md w-full mb-6 sm:w-fit md:w-[49%] lg:w-[32.7%]">
-        <img class="w-full rounded-t-2xl object-fill" :src="'https://res.cloudinary.com/dsgvwxygr/image/upload/v1678715023/photo_2023-03-13_14-43-12_lpr53t.jpg'" :alt="'image - Late Engr Jojo Udensi'">
-        <div class="p-6">
-          <h3 class="text-xl font-semibold">Late Engr Jojo Udensi</h3>
-          <p class="text-slate-500 mt-6 text-sm leading-7">Director of Evangelism Lagos Congress</p>
-        </div>
+  <div class="flex justify-center gap-2 px-4 sm:px-8 md:px-12 lg:px-28 py-24 transition-all">
+    <div class="rounded-t-2xl bg-white transition-all hover:shadow-md w-full mb-6 sm:w-fit ">
+      <img class="w-full rounded-t-2xl object-fill h-96" :src="'https://res.cloudinary.com/dsgvwxygr/image/upload/v1678715023/photo_2023-03-13_14-43-12_lpr53t.jpg'" :alt="'image - Late Engr Jojo Udensi'">
+      <div class="p-6">
+        <h3 class="text-xl font-semibold">Late Engr Jojo Udensi</h3>
+        <p class="text-slate-500 mt-6 text-sm leading-7">Director of Evangelism Lagos Congress</p>
       </div>
     </div>
-    <div class="w-full md:flex flex-wrap justify-between">
-      <div class="rounded-t-2xl bg-white transition-all hover:shadow-md w-full mb-6 sm:w-fit md:w-[49%] lg:w-[32.7%]">
-        <img class="w-full rounded-t-2xl object-fill" :src="'https://res.cloudinary.com/dsgvwxygr/image/upload/v1678715022/photo_2023-03-13_14-43-14_umy2c7.jpg'" :alt="'image - Late Okey Emenike'">
-        <div class="p-6">
-          <h3 class="text-xl font-semibold">Late Okey Emenike</h3>
-          <p class="text-slate-500 mt-6 text-sm leading-7">First National President President</p>
-        </div>
+    <div class="rounded-t-2xl bg-white transition-all hover:shadow-md w-full mb-6 sm:w-fit ">
+      <img class="w-full rounded-t-2xl object-fill h-96" :src="'https://res.cloudinary.com/dsgvwxygr/image/upload/v1678715022/photo_2023-03-13_14-43-14_umy2c7.jpg'" :alt="'image - Late Okey Emenike'">
+      <div class="p-6">
+        <h3 class="text-xl font-semibold">Late Okey Emenike</h3>
+        <p class="text-slate-500 mt-6 text-sm leading-7">First National President President</p>
       </div>
     </div>
   </div>
