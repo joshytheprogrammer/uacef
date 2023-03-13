a794303da4b11364cadd403b5b539d15b10fbb1d
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-03-13 12:08:14 +0100

    text change (2)

--- components/Home/Jumbotron.vue
+++ components/Home/Jumbotron.vue
@@ -4,7 +4,7 @@
       <h1 class="text-5xl lg:text-7xl font-bold">Building</h1>
       <h4 class="text-xl lg:text-4xl font-bold pt-2">4 a better Nigeria</h4>
     </div>
-    <p class="py-4 leading-7 lg:pb-12 md:w-full lg:w-1/2">The United Akokwa Christian Elites Forum is a non denominational forum with a special assignment for our land and our generation - Helping the needy, blessing the poor and putting smiles on the faces of our countrymen. We have been working at it for over <span class="font-semibold">2 DECADES</span> and we won't stop anytime soon. </p>
+    <p class="py-4 leading-7 lg:pb-12 md:w-full lg:w-1/2">The United Akokwa Christian Elites Forum is a non denominational forum with a special assignment for our clan and our generation - Helping the needy, blessing the poor and putting smiles on the faces of our countrymen. We have been working at it for over <span class="font-semibold">2 DECADES</span> and we won't stop anytime soon. </p>
     <div class="flex pt-4 sm:pt-0">
       <NuxtLink to="/contact" class="block bg-purple-900 rounded-2xl px-4 sm:px-8 py-4 mr-4">Join us</NuxtLink>
       <NuxtLink to="/about" class="block hover:bg-purple-900 border-2 border-purple-900 rounded-2xl px-4 sm:px-8 py-4 mr-4">Read More</NuxtLink>
