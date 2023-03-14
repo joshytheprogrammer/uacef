abef5549e5a04d2cbb5af4cff4503bb900afa1e6
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-03-14 10:42:31 +0100

    made changes

--- components/Contact/Maps.vue
+++ components/Contact/Maps.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d31713.19989732525!2d3.3642907999999996!3d6.502689449999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sng!4v1678266019267!5m2!1sen!2sng" width="100%" height="450" style="border:0;" allowfullscreen="false" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d254050.4367371381!2d6.9251185810655835!3d5.779387503519597!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1043093d1cf341f5%3A0x806c272a2e371209!2sAkokwa%20Unity%20Centre!5e0!3m2!1sen!2sng!4v1678786867639!5m2!1sen!2sng" width="100%" height="450" style="border:0;" allowfullscreen="false" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
   </div>
 </template>
