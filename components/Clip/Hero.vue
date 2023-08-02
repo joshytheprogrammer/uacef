5e67cbd77660ce3a22476cfa522dcdf5dbf69710
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-08-02 14:13:46 +0100

    made changes: 'built hero page'

---
+++ components/Clip/Hero.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="bg-gradient-to-r from-purple-900 via-purple-800 to-purple-700 text-white flex items-center py-8 px-4 sm:px-8 md:px-12 lg:px-28 lg:py-24 h-fit md:h-[265px] ">
+    <h1 class="text-2xl md:text-4xl pb-6 font-semibold relative ">Experience the moment with us</h1>
+  </div>
+</template>
--- pages/clips.vue
+++ pages/clips.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="bg-gradient-to-r from-purple-900 via-purple-800 to-purple-700 min-h-screen flex items-center justify-center">
-    <AppTease />
+  <div class="">
+    <ClipHero />
   </div>
 </template>
 
