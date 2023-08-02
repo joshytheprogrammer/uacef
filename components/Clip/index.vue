725893acec21ed133599af5937f3d45f686fd2a0
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-08-02 16:45:16 +0100

    made changes: 'made clips dynamic'

--- components/Clip/index.vue
+++ components/Clip/index.vue
@@ -1,11 +1,20 @@
 <template>
   <div class="md:columns-2 gap-6 px-4 sm:px-8 md:px-12 lg:px-28 py-24 transition-all">
-    <iframe src="https://www.youtube.com/embed/q4DF3j4saCE?" class="w-full p-1 shadow-md mr-6 mb-6 rounded-xl inline-block border-2 border-solid border-gray-400" width="480" height="360" frameborder="0" loading="lazy" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <iframe v-for="item in videos" :key="item.id" :src="'https://www.youtube.com/embed/'+item.vidID" class="w-full p-1 shadow-md mr-6 mb-6 rounded-xl inline-block border-2 border-solid border-gray-400" width="480" height="360" frameborder="0" loading="lazy" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
   </div>
 </template>
 
 <script>
   export default {
-    
+    data() {
+      return {
+        videos: [
+          {
+            id: (Math.floor(Math.random() * 10) + Math.random()),
+            vidID: 'q4DF3j4saCE'
+          }
+        ]
+      }
+    }
   }
 </script>
