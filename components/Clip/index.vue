c4f7ff343f81cd5c0344b18c04b5230f5182971f
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-08-14 16:28:52 +0100

    made changes: 'made clips setup'

--- components/Clip/index.vue
+++ components/Clip/index.vue
@@ -4,109 +4,103 @@
   </div>
 </template>
 
-<script>
-  export default {
-    data() {
-      return {
-        videos: [
-          {
-            id: (Math.floor(Math.random() * 10) + Math.random()),
-            vidID: 'vjFO0Y-jWDs'
-          },
-          {
-            id: (Math.floor(Math.random() * 10) + Math.random()),
-            vidID: 'ubJlMMGw2Wo'
-          },
-          {
-            id: (Math.floor(Math.random() * 10) + Math.random()),
-            vidID: '62-CLiV3Dq0'
-          },
-          {
-            id: (Math.floor(Math.random() * 10) + Math.random()),
-            vidID: '1Ia0h8ZqRSM'
-          },
-          {
-            id: (Math.floor(Math.random() * 10) + Math.random()),
-            vidID: 'GecV9AUzZ1M'
-          },
-          {
-            id: (Math.floor(Math.random() * 10) + Math.random()),
-            vidID: 'HXNH6O5e9Xs'
-          },
-          {
-            id: (Math.floor(Math.random() * 10) + Math.random()),
-            vidID: 'dXEngk41laA'
-          },
-          {
-            id: (Math.floor(Math.random() * 10) + Math.random()),
-            vidID: 'NKjmOHmo7lw'
-          },
-          {
-            id: (Math.floor(Math.random() * 10) + Math.random()),
-            vidID: 'qdf7pyvzuWU'
-          },
-          {
-            id: (Math.floor(Math.random() * 10) + Math.random()),
-            vidID: 'LZRyRH0vy8Y'
-          },
-          {
-            id: (Math.floor(Math.random() * 10) + Math.random()),
-            vidID: '9alQqAmPISg'
-          },
-          {
-            id: (Math.floor(Math.random() * 10) + Math.random()),
-            vidID: 'o15VBTKyTcs'
-          },
-          {
-            id: (Math.floor(Math.random() * 10) + Math.random()),
-            vidID: 'aCzfZ64uyQw'
-          },
-          {
-            id: (Math.floor(Math.random() * 10) + Math.random()),
-            vidID: 'o4h2CZgpfy4'
-          },
-          {
-            id: (Math.floor(Math.random() * 10) + Math.random()),
-            vidID: '5p8eghZJgKo'
-          },
-          {
-            id: (Math.floor(Math.random() * 10) + Math.random()),
-            vidID: 'HxvO5W7huMY'
-          },
-          {
-            id: (Math.floor(Math.random() * 10) + Math.random()),
-            vidID: 'VBdBW2zF4Js'
-          },
-          {
-            id: (Math.floor(Math.random() * 10) + Math.random()),
-            vidID: 'sW5_SbcA6fE'
-          },
-          {
-            id: (Math.floor(Math.random() * 10) + Math.random()),
-            vidID: '9PRcoTUxVGk'
-          },
-          {
-            id: (Math.floor(Math.random() * 10) + Math.random()),
-            vidID: '4VOtS4OBQxs'
-          },
-          {
-            id: (Math.floor(Math.random() * 10) + Math.random()),
-            vidID: 'ZyAlE4U7CxA'
-          },
-          {
-            id: (Math.floor(Math.random() * 10) + Math.random()),
-            vidID: 'dImo86d3RZ4'
-          },
-          {
-            id: (Math.floor(Math.random() * 10) + Math.random()),
-            vidID: 'nARkgybU-uQ'
-          },
-          {
-            id: (Math.floor(Math.random() * 10) + Math.random()),
-            vidID: 'LFt7cvZwh88'
-          },
-        ]
-      }
-    }
-  }
+<script setup>
+  const videos = [
+    {
+      id: (Math.floor(Math.random() * 10) + Math.random()),
+      vidID: 'vjFO0Y-jWDs'
+    },
+    {
+      id: (Math.floor(Math.random() * 10) + Math.random()),
+      vidID: 'ubJlMMGw2Wo'
+    },
+    {
+      id: (Math.floor(Math.random() * 10) + Math.random()),
+      vidID: '62-CLiV3Dq0'
+    },
+    {
+      id: (Math.floor(Math.random() * 10) + Math.random()),
+      vidID: '1Ia0h8ZqRSM'
+    },
+    {
+      id: (Math.floor(Math.random() * 10) + Math.random()),
+      vidID: 'GecV9AUzZ1M'
+    },
+    {
+      id: (Math.floor(Math.random() * 10) + Math.random()),
+      vidID: 'HXNH6O5e9Xs'
+    },
+    {
+      id: (Math.floor(Math.random() * 10) + Math.random()),
+      vidID: 'dXEngk41laA'
+    },
+    {
+      id: (Math.floor(Math.random() * 10) + Math.random()),
+      vidID: 'NKjmOHmo7lw'
+    },
+    {
+      id: (Math.floor(Math.random() * 10) + Math.random()),
+      vidID: 'qdf7pyvzuWU'
+    },
+    {
+      id: (Math.floor(Math.random() * 10) + Math.random()),
+      vidID: 'LZRyRH0vy8Y'
+    },
+    {
+      id: (Math.floor(Math.random() * 10) + Math.random()),
+      vidID: '9alQqAmPISg'
+    },
+    {
+      id: (Math.floor(Math.random() * 10) + Math.random()),
+      vidID: 'o15VBTKyTcs'
+    },
+    {
+      id: (Math.floor(Math.random() * 10) + Math.random()),
+      vidID: 'aCzfZ64uyQw'
+    },
+    {
+      id: (Math.floor(Math.random() * 10) + Math.random()),
+      vidID: 'o4h2CZgpfy4'
+    },
+    {
+      id: (Math.floor(Math.random() * 10) + Math.random()),
+      vidID: '5p8eghZJgKo'
+    },
+    {
+      id: (Math.floor(Math.random() * 10) + Math.random()),
+      vidID: 'HxvO5W7huMY'
+    },
+    {
+      id: (Math.floor(Math.random() * 10) + Math.random()),
+      vidID: 'VBdBW2zF4Js'
+    },
+    {
+      id: (Math.floor(Math.random() * 10) + Math.random()),
+      vidID: 'sW5_SbcA6fE'
+    },
+    {
+      id: (Math.floor(Math.random() * 10) + Math.random()),
+      vidID: '9PRcoTUxVGk'
+    },
+    {
+      id: (Math.floor(Math.random() * 10) + Math.random()),
+      vidID: '4VOtS4OBQxs'
+    },
+    {
+      id: (Math.floor(Math.random() * 10) + Math.random()),
+      vidID: 'ZyAlE4U7CxA'
+    },
+    {
+      id: (Math.floor(Math.random() * 10) + Math.random()),
+      vidID: 'dImo86d3RZ4'
+    },
+    {
+      id: (Math.floor(Math.random() * 10) + Math.random()),
+      vidID: 'nARkgybU-uQ'
+    },
+    {
+      id: (Math.floor(Math.random() * 10) + Math.random()),
+      vidID: 'LFt7cvZwh88'
+    },
+  ]
 </script>
