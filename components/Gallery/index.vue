29488ee3e93a9b8f435a9db2360a86df4781c6ac
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-08-14 18:02:38 +0100

    made changes: 'added Loader.vue'

--- components/Gallery/index.vue
+++ components/Gallery/index.vue
@@ -1,6 +1,6 @@
 <template>
   <section>
-    <div v-if="isLoading" class="text-center mt-8">Loading...</div>
+    <AppLoader v-if="isLoading" />
     <div v-else-if="error" class="text-center mt-8 text-red-500">{{ error }}</div>
     <div v-else class="md:columns-2 lg:columns-3 gap-6 px-4 sm:px-8 md:px-12 lg:px-28 py-24 transition-all">
       <figure class="w-full p-1 shadow-md mr-6 mb-6 rounded-xl inline-block border-2 border-solid border-gray-400 hover:border-purple-900" v-for="item in images" :key="item.id">
