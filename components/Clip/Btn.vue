94dc2645690381812a80e3d26d0334330d91a18c
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-08-16 16:20:19 +0100

    made changes: 'attempting to resolve merge conflicts'

---
+++ components/Clip/Btn.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="px-4 sm:px-8 md:px-12 lg:px-28 py-24 w-full">
+    <a class="block w-fit text-white bg-purple-900 border-2 border-purple-900 rounded-2xl px-4 sm:px-8 py-4 m-auto" href="https://youtube.com/@uacef">See More</a>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
