9ba06cc3318b727275aaa83abd0480a7d6b8ced5
joshytheprogrammer <jurassicjoshua@protonmail.com>
2023-08-16 16:36:00 +0100

    made changes: 'fixed all hiding issue'

--- components/Clip/index.vue
+++ components/Clip/index.vue
@@ -10,7 +10,7 @@
 </template>
 
 <script setup>
-import { getDocs, collection } from 'firebase/firestore';
+import { getDocs, collection, where, query } from 'firebase/firestore';
 
 const nuxtApp = useNuxtApp();
 const db = nuxtApp.$firestore;
@@ -20,9 +20,10 @@ const error = ref(null);
 
 onMounted(async () => {
   try {
-    const querySnapshot = await getDocs(collection(db, 'clip'));
+    const q = query(collection(db, 'clip'), where("active", "==", true))
+    const querySnapshot = await getDocs(q);
     querySnapshot.forEach((doc) => {
-      // Push each document data into the images array
+      // Push each document data into the clips array
       videos.value.push({
         id: doc.id,
         vidID: doc.data().videoID,
@@ -36,102 +37,4 @@ onMounted(async () => {
   }
 });
 
-// const videos = [
-//   {
-//     id: (Math.floor(Math.random() * 10) + Math.random()),
-//     vidID: 'vjFO0Y-jWDs'
-//   },
-//   {
-//     id: (Math.floor(Math.random() * 10) + Math.random()),
-//     vidID: 'ubJlMMGw2Wo'
-//   },
-//   {
-//     id: (Math.floor(Math.random() * 10) + Math.random()),
-//     vidID: '62-CLiV3Dq0'
-//   },
-//   {
-//     id: (Math.floor(Math.random() * 10) + Math.random()),
-//     vidID: '1Ia0h8ZqRSM'
-//   },
-//   {
-//     id: (Math.floor(Math.random() * 10) + Math.random()),
-//     vidID: 'GecV9AUzZ1M'
-//   },
-//   {
-//     id: (Math.floor(Math.random() * 10) + Math.random()),
-//     vidID: 'HXNH6O5e9Xs'
-//   },
-//   {
-//     id: (Math.floor(Math.random() * 10) + Math.random()),
-//     vidID: 'dXEngk41laA'
-//   },
-//   {
-//     id: (Math.floor(Math.random() * 10) + Math.random()),
-//     vidID: 'NKjmOHmo7lw'
-//   },
-//   {
-//     id: (Math.floor(Math.random() * 10) + Math.random()),
-//     vidID: 'qdf7pyvzuWU'
-//   },
-//   {
-//     id: (Math.floor(Math.random() * 10) + Math.random()),
-//     vidID: 'LZRyRH0vy8Y'
-//   },
-//   {
-//     id: (Math.floor(Math.random() * 10) + Math.random()),
-//     vidID: '9alQqAmPISg'
-//   },
-//   {
-//     id: (Math.floor(Math.random() * 10) + Math.random()),
-//     vidID: 'o15VBTKyTcs'
-//   },
-//   {
-//     id: (Math.floor(Math.random() * 10) + Math.random()),
-//     vidID: 'aCzfZ64uyQw'
-//   },
-//   {
-//     id: (Math.floor(Math.random() * 10) + Math.random()),
-//     vidID: 'o4h2CZgpfy4'
-//   },
-//   {
-//     id: (Math.floor(Math.random() * 10) + Math.random()),
-//     vidID: '5p8eghZJgKo'
-//   },
-//   {
-//     id: (Math.floor(Math.random() * 10) + Math.random()),
-//     vidID: 'HxvO5W7huMY'
-//   },
-//   {
-//     id: (Math.floor(Math.random() * 10) + Math.random()),
-//     vidID: 'VBdBW2zF4Js'
-//   },
-//   {
-//     id: (Math.floor(Math.random() * 10) + Math.random()),
-//     vidID: 'sW5_SbcA6fE'
-//   },
-//   {
-//     id: (Math.floor(Math.random() * 10) + Math.random()),
-//     vidID: '9PRcoTUxVGk'
-//   },
-//   {
-//     id: (Math.floor(Math.random() * 10) + Math.random()),
-//     vidID: '4VOtS4OBQxs'
-//   },
-//   {
-//     id: (Math.floor(Math.random() * 10) + Math.random()),
-//     vidID: 'ZyAlE4U7CxA'
-//   },
-//   {
-//     id: (Math.floor(Math.random() * 10) + Math.random()),
-//     vidID: 'dImo86d3RZ4'
-//   },
-//   {
-//     id: (Math.floor(Math.random() * 10) + Math.random()),
-//     vidID: 'nARkgybU-uQ'
-//   },
-//   {
-//     id: (Math.floor(Math.random() * 10) + Math.random()),
-//     vidID: 'LFt7cvZwh88'
-//   },
-// ];
 </script>
